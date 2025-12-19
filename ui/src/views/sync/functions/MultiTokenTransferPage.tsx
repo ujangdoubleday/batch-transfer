f37f591eb4012f7bea3e5c70eae26d976f75f543
ujangdoubleday <domba88galing@gmail.com>
2025-12-19 18:29:57 +0700

    style: unify page layout width

--- ui/src/views/sync/functions/MultiTokenTransferPage.tsx
+++ ui/src/views/sync/functions/MultiTokenTransferPage.tsx
@@ -7,7 +7,7 @@ export function MultiTokenTransferPage() {
   const { address } = useParams<{ address: string }>();
   
   return (
-    <FunctionPage title="Multi-Token Transfer">
+    <FunctionPage title="Multi-Token Transfer" maxWidth="max-w-7xl" isCard={false}>
         <BatchTransferMultiTokensForm contractAddress={address as Address} />
     </FunctionPage>
   );
